447957813420756c42cce45569e5bc196317d1c4
Benjamin S. Kirk <benjamin.kirk@nasa.gov>
2012-10-09 08:34:07 -0500

    more TeX utils

--- it/scripts/mac.sh
+++ it/scripts/mac.sh
@@ -20,7 +20,12 @@ port install git-core +subversion
 port install emacs-app color-theme-mode.el
 
 # compilers, mpi, latex, & development libraries
-port install openmpi tbb subversion git-core +svn git-extras gcc_select gmake autoconf automake libtool texlive-latex texlive-latex-extra texlive-latex-recommended bash bash-completion texlive-fonts-recommended texlive-fontutils boost cgal eigen3 scons gsl doxygen graphviz gsed hdf5-18 +fortran +gcc45 clang-3.1 clang_select glpk vtk5 ImageMagick
+port install openmpi tbb subversion git-core +svn git-extras gcc_select gmake autoconf automake libtool texlive-latex texlive-latex-extra texlive-latex-recommended bash bash-completion texlive-fonts-recommended texlive-fontutils texlive-bin-extra boost cgal eigen3 scons gsl doxygen graphviz gsed clang-3.1 clang_select glpk vtk5 
+
+port install hdf5-18 +fortran +gcc45
+
+# graphics utilities, useful with latex
+port install ImageMagick inkscape
 
 # set the default gcc and clang
 port select gcc mp-gcc45
@@ -38,6 +43,13 @@ mkdir -p /opt/local/var/macports/build/_opt_local_var_macports_sources_svn.macpo
 cd /opt/local/bin
 for file in mpirun mpicc mpicxx mpif77 mpif90 ; do ln -s open$file $file ; ls -l $file ; done
 cd -
+
+
+# prefer some macports commands
+cd /opt/local/bin
+ln -s gln ln && ls -l ln
+cd -
+
 
 
 
