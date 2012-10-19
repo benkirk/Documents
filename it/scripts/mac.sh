c4ba6e2213969e281a2c1585aa94e46f0a29ff16
Benjamin S. Kirk <benjamin.kirk@nasa.gov>
2012-10-19 09:35:45 -0500

    gnuplot

--- it/scripts/mac.sh
+++ it/scripts/mac.sh
@@ -15,17 +15,15 @@
 
 port install python27
 
-port install git-core +subversion
-
 port install emacs-app color-theme-mode.el
 
 # compilers, mpi, latex, & development libraries
-port install openmpi tbb subversion git-core +svn git-extras gcc_select gmake autoconf automake libtool texlive-latex texlive-latex-extra texlive-latex-recommended bash bash-completion texlive-fonts-recommended texlive-fontutils texlive-bin-extra boost cgal eigen3 scons gsl doxygen graphviz gsed clang-3.1 clang_select glpk vtk5 
+port install openmpi tbb subversion git-core +svn git-extras gcc_select gmake autoconf automake libtool texlive-latex texlive-latex-extra texlive-latex-recommended bash bash-completion texlive-fonts-recommended texlive-fontutils texlive-bin-extra texlive-generic-recommended boost cgal eigen3 scons gsl doxygen graphviz gsed clang-3.1 clang_select glpk vtk5 dos2unix
 
 port install hdf5-18 +fortran +gcc45
 
 # graphics utilities, useful with latex
-port install ImageMagick inkscape
+port install ImageMagick inkscape dia
 
 # set the default gcc and clang
 port select gcc mp-gcc45
@@ -34,9 +32,11 @@ port select clang mp-clang-3.1
 # petsc
 port install petsc slepc
 
+# gnuplot
+port install gnuplot
+
 # octave and its packages
 port install octave octave-general octave-gsl octave-image octave-io octave-linear-algebra octave-missing-functions octave-msh octave-optiminterp octave-optim octave-plot octave-specfun octave-splines octave-statistics octave-strings octave-struct octave-physicalconstants octave-odepkg
-
 
 # make symbolic links to OpenMPI commands
 mkdir -p /opt/local/var/macports/build/_opt_local_var_macports_sources_svn.macports.org_trunk_dports_science_openmpi/openmpi/work/build/opal/mca/hwloc/hwloc132/hwloc/src
