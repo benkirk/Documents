5b0fdac84c583ae79839cd54681d11647e15fecf
Benjamin S. Kirk <benjamin.kirk@nasa.gov>
2012-10-28 12:34:51 -0500

    macports for rebuild

--- it/scripts/mac.sh
+++ it/scripts/mac.sh
@@ -13,21 +13,26 @@
 #  sudo port sync -d
 #  sudo port upgrade outdated
 
-port install python27
+
+# clang first - among other things, seesm to install ld64, and that helps
+# some more fragile ports work later
+port install clang-3.1 clang_select
+port select clang mp-clang-3.1
 
 port install emacs-app color-theme-mode.el
 
 # compilers, mpi, latex, & development libraries
-port install openmpi tbb subversion git-core +svn git-extras gcc_select gmake autoconf automake libtool texlive-latex texlive-latex-extra texlive-latex-recommended bash bash-completion texlive-fonts-recommended texlive-fontutils texlive-bin-extra texlive-generic-recommended boost cgal eigen3 scons gsl doxygen graphviz gsed clang-3.1 clang_select glpk vtk5 dos2unix
+port install gcc45 gcc_select 
+
+port install openmpi tbb subversion git-core +svn git-extras gmake autoconf automake libtool texlive-latex texlive-latex-extra texlive-latex-recommended bash bash-completion texlive-fonts-recommended texlive-fontutils texlive-bin-extra texlive-generic-recommended boost cgal eigen3 scons gsl doxygen graphviz gsed vtk5 dos2unix
+
+# set the default gcc
+port select gcc mp-gcc45
 
 port install hdf5-18 +fortran +gcc45
 
 # graphics utilities, useful with latex
 port install ImageMagick inkscape dia aspell aspell-dict-en
-
-# set the default gcc and clang
-port select gcc mp-gcc45
-port select clang mp-clang-3.1
 
 # petsc
 port install petsc slepc
@@ -36,17 +41,15 @@ port install petsc slepc
 port install gnuplot
 
 # octave and its packages
-port install octave octave-general octave-gsl octave-image octave-io octave-linear-algebra octave-missing-functions octave-msh octave-optiminterp octave-optim octave-plot octave-specfun octave-splines octave-statistics octave-strings octave-struct octave-physicalconstants octave-odepkg
+#port install octave octave-general octave-gsl octave-io octave-linear-algebra octave-missing-functions octave-msh octave-optim octave-plot octave-specfun octave-splines octave-statistics octave-strings octave-struct octave-physicalconstants octave-odepkg
 
 # make symbolic links to OpenMPI commands
 mkdir -p /opt/local/var/macports/build/_opt_local_var_macports_sources_svn.macports.org_trunk_dports_science_openmpi/openmpi/work/build/opal/mca/hwloc/hwloc132/hwloc/src
 cd /opt/local/bin
-for file in mpirun mpicc mpicxx mpif77 mpif90 ; do ln -s open$file $file ; ls -l $file ; done
-cd -
-
-
+for file in mpirun mpicc mpicxx mpif77 mpif90 ; do 
+    ln -s open$file $file ; ls -l $file 
+done
 # prefer some macports commands
-cd /opt/local/bin
 ln -s gln ln && ls -l ln
 cd -
 
