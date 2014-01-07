86e688432a674b0c2b6f6e6677899fa28488209b
Benjamin S. Kirk <benjamin.kirk@nasa.gov>
2014-01-07 14:31:20 -0600

    updating for recent macports

--- it/scripts/mac.sh
+++ it/scripts/mac.sh
@@ -5,57 +5,54 @@
 
 export PATH=/opt/local/bin:/opt/local/sbin:$PATH
 
-# # Setup macports to use a local port config instead of rsync'ing
-# # the source, so it works on site
-# cd /opt/local/var/macports/sources
-# mkdir -p svn.macports.org/trunk/dports
-# cd svn.macports.org/trunk/dports
-# svn co http://svn.macports.org/repository/macports/trunk/dports/ .
+if (! test -d /opt/local/var/macports/sources/svn.macports.org/trunk/dports); then
+  # Setup macports to use a local port config instead of rsync'ing
+  # the source, so it works on site
+  cd /opt/local/var/macports/sources
+  mkdir -p svn.macports.org/trunk/dports
+  cd svn.macports.org/trunk/dports
+  svn co http://svn.macports.org/repository/macports/trunk/dports/ .
 
-# cp /opt/local/etc/macports/sources.conf /opt/local/etc/macports/sources.conf.orig
-# cat << EOF >/opt/local/etc/macports/sources.conf
-# #  MacPorts system wide sources configuration file
-# #  $Id: sources.conf 79599 2011-06-19 21:18:18Z jmr@macports.org $
+  cp /opt/local/etc/macports/sources.conf /opt/local/etc/macports/sources.conf.orig
+  cat << EOF >/opt/local/etc/macports/sources.conf
+#  MacPorts system wide sources configuration file
+#  $Id: sources.conf 79599 2011-06-19 21:18:18Z jmr@macports.org $
 
-# #  To setup a local ports repository, insert a "file://" entry following
-# #  the example below that points to your local ports directory:
-# #  Example: file:///Users/landonf/misc/MacPorts/ports
+#  To setup a local ports repository, insert a "file://" entry following
+#  the example below that points to your local ports directory:
+#  Example: file:///Users/landonf/misc/MacPorts/ports
 
-# #  The default MacPorts repository should always be tagged [default]
-# #  for proper functionality of various resources (port groups, mirror
-# #  sites, etc).  If you switch it from the rsync:// URL, be sure to keep
-# #  it tagged [default].
+#  The default MacPorts repository should always be tagged [default]
+#  for proper functionality of various resources (port groups, mirror
+#  sites, etc).  If you switch it from the rsync:// URL, be sure to keep
+#  it tagged [default].
 
-# #  To prevent a source from synchronizing when `port sync` is used,
-# #  append [nosync] at the end as shown in this example:
-# #  Example: file:///Users/landonf/misc/MacPorts/ports [nosync]
+#  To prevent a source from synchronizing when `port sync` is used,
+#  append [nosync] at the end as shown in this example:
+#  Example: file:///Users/landonf/misc/MacPorts/ports [nosync]
 
-# #  NOTE: The port command parses source URLs in order and installs the
-# #        first occurrance when a port appears in multiple repositories.
-# #        So keep "file://" URLs above other URL types.
+#  NOTE: The port command parses source URLs in order and installs the
+#        first occurrance when a port appears in multiple repositories.
+#        So keep "file://" URLs above other URL types.
 
 
-# #  To get the ports tree from the master MacPorts server in California, USA use:
-# #      rsync://rsync.macports.org/release/ports/
-# #  To get it from the mirror in Trondheim, Norway use:
-# #      rsync://trd.no.rsync.macports.org/release/ports/
-# #  A current list of mirrors is available at https://trac.macports.org/wiki/Mirrors
+#  To get the ports tree from the master MacPorts server in California, USA use:
+#      rsync://rsync.macports.org/release/ports/
+#  To get it from the mirror in Trondheim, Norway use:
+#      rsync://trd.no.rsync.macports.org/release/ports/
+#  A current list of mirrors is available at https://trac.macports.org/wiki/Mirrors
 
-# # If an rsync URL points to a .tar file, a signed .rmd160 must exist next to
-# # it on the server and will be used to verify its integrity.
+# If an rsync URL points to a .tar file, a signed .rmd160 must exist next to
+# it on the server and will be used to verify its integrity.
 
-# #rsync://rsync.macports.org/release/tarballs/ports.tar [default]
-# file:///opt/local/var/macports/sources/svn.macports.org/trunk/dports/ [default]
+#rsync://rsync.macports.org/release/tarballs/ports.tar [default]
+file:///opt/local/var/macports/sources/svn.macports.org/trunk/dports/ [default]
 
-# EOF
+EOF
 
-# port -d sync
+  port -d sync
 
-# First install:
-# --------------
-#
-#
-#
+fi
 
 # To update:
 # ----------
@@ -67,8 +64,8 @@ export PATH=/opt/local/bin:/opt/local/sbin:$PATH
 
 # clang first - among other things, seesm to install ld64, and that helps
 # some more fragile ports work later
-port install clang-3.4 clang_select
-port select clang mp-clang-3.4
+port install clang-3.5 clang_select
+port select clang mp-clang-3.5
 
 port install emacs-app color-theme-mode.el
 
@@ -79,7 +76,7 @@ port select gcc mp-gcc48
 port install hdf5-18 +fortran +gcc48
 
 # mpi, latex, & development libraries
-port install openmpi tbb subversion git-core +svn  gmake autoconf automake libtool texlive-latex texlive-latex-extra texlive-latex-recommended bash bash-completion texlive-fonts-recommended texlive-fontutils texlive-bin-extra texlive-generic-recommended boost eigen3 scons gsl doxygen graphviz gsed dos2unix vtk5 cgal # valgrind git-extras
+port install openmpi tbb subversion git-core +svn  gmake autoconf automake libtool texlive-latex texlive-latex-extra texlive-latex-recommended bash bash-completion texlive-fonts-recommended texlive-fontutils texlive-bin-extra texlive-generic-recommended boost eigen3 scons gsl doxygen graphviz gsed dos2unix vtk5 cgal git-extras #valgrind
 
 # petsc
 #port install petsc slepc
@@ -91,24 +88,21 @@ port install ImageMagick inkscape dia aspell aspell-dict-en
 port install gnuplot
 
 # lua and requisites for lmod
-port install lua lua-luafilesystem luarocks
-luarocks install luaposix
+#port install lua lua-luafilesystem luarocks
+#luarocks install luaposix
 
 port install synergy
 
 # octave and its packages
 #port install atlas +gcc48
 port install octave +accelerate +gcc48 -atlas
-#port install octave-general octave-gsl octave-io octave-linear-algebra octave-missing-functions octave-msh octave-optim octave-plot octave-specfun octave-splines octave-statistics octave-strings octave-struct octave-physicalconstants octave-odepkg
+port install octave-general octave-gsl octave-io octave-linear-algebra octave-missing-functions octave-msh octave-optim octave-plot octave-specfun octave-splines octave-statistics octave-strings octave-struct octave-physicalconstants octave-odepkg
 
 # gimp & friends
 port install gimp2 gimp-print macfile-gimp macclipboard-gimp
 
-# x11
-port install xorg-server
 
 # make symbolic links to OpenMPI commands
-#mkdir -p /opt/local/var/macports/build/_opt_local_var_macports_sources_svn.macports.org_trunk_dports_science_openmpi/openmpi/work/build/opal/mca/hwloc/hwloc132/hwloc/src
 cd /opt/local/bin
 for file in mpirun mpicc mpicxx mpif77 mpif90 ; do
     ln -s open$file $file ; ls -l $file
