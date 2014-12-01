206a6fd24ee65cac1c31f1a3fc65285997b481f0
Benjamin S. Kirk <benjamin.kirk@nasa.gov>
2014-12-01 08:52:10 -0600

    Yosemite + macports

--- it/scripts/mac.sh
+++ it/scripts/mac.sh
@@ -5,62 +5,6 @@
 
 export PATH=/opt/local/bin:/opt/local/sbin:$PATH
 
-if (! test -d /opt/local/var/macports/sources/svn.macports.org/trunk/dports); then
-  # Setup macports to use a local port config instead of rsync'ing
-  # the source, so it works on site
-  cd /opt/local/var/macports/sources
-  mkdir -p svn.macports.org/trunk/dports
-  cd svn.macports.org/trunk/dports
-  svn co http://svn.macports.org/repository/macports/trunk/dports/ .
-
-  cp /opt/local/etc/macports/sources.conf /opt/local/etc/macports/sources.conf.orig
-  cat << EOF >/opt/local/etc/macports/sources.conf
-#  MacPorts system wide sources configuration file
-#  $Id: sources.conf 79599 2011-06-19 21:18:18Z jmr@macports.org $
-
-#  To setup a local ports repository, insert a "file://" entry following
-#  the example below that points to your local ports directory:
-#  Example: file:///Users/landonf/misc/MacPorts/ports
-
-#  The default MacPorts repository should always be tagged [default]
-#  for proper functionality of various resources (port groups, mirror
-#  sites, etc).  If you switch it from the rsync:// URL, be sure to keep
-#  it tagged [default].
-
-#  To prevent a source from synchronizing when `port sync` is used,
-#  append [nosync] at the end as shown in this example:
-#  Example: file:///Users/landonf/misc/MacPorts/ports [nosync]
-
-#  NOTE: The port command parses source URLs in order and installs the
-#        first occurrance when a port appears in multiple repositories.
-#        So keep "file://" URLs above other URL types.
-
-
-#  To get the ports tree from the master MacPorts server in California, USA use:
-#      rsync://rsync.macports.org/release/ports/
-#  To get it from the mirror in Trondheim, Norway use:
-#      rsync://trd.no.rsync.macports.org/release/ports/
-#  A current list of mirrors is available at https://trac.macports.org/wiki/Mirrors
-
-# If an rsync URL points to a .tar file, a signed .rmd160 must exist next to
-# it on the server and will be used to verify its integrity.
-
-#rsync://rsync.macports.org/release/tarballs/ports.tar [default]
-file:///opt/local/var/macports/sources/svn.macports.org/trunk/dports/ [default]
-
-EOF
-
-  port -d sync
-
-fi
-
-# # To update:
-# # ----------
-# #  cd /opt/local/var/macports/sources/svn.macports.org/trunk/dports/
-# #  sudo svn up
-# #  sudo port sync -d
-# #  sudo port upgrade outdated
-
 
 # clang first - among other things, seesm to install ld64, and that helps
 # some more fragile ports work later
@@ -72,16 +16,16 @@ port select --set python python27
 port install emacs-app color-theme-mode.el
 
 # set the default gcc
-port install gcc48 gcc_select
-port select --set gcc mp-gcc48
+port install gcc49 gcc_select
+port select --set gcc mp-gcc49
 
-port install hdf5-18 +fortran +gcc48
+port install hdf5 +fortran +gcc49
 
 # mpi, latex, & development libraries
-port install openmpi-gcc48 mpi_select
-port select --set mpi openmpi-gcc48-fortran
+port install openmpi-gcc49 mpi_select
+port select --set mpi openmpi-gcc49-fortran
 
-port install tbb subversion git-core +svn  gmake autoconf automake libtool texlive-latex texlive-latex-extra texlive-latex-recommended bash bash-completion texlive-fonts-recommended texlive-fontutils texlive-bin-extra texlive-generic-recommended texlive-math-extra boost eigen3 scons gsl doxygen graphviz gsed dos2unix vtk5 cgal git-extras
+port install tbb subversion git +svn  gmake autoconf automake libtool texlive-latex texlive-latex-extra texlive-latex-recommended bash bash-completion texlive-fonts-recommended texlive-fontutils texlive-bin-extra texlive-generic-recommended texlive-math-extra boost eigen3 scons gsl doxygen graphviz gsed dos2unix vtk5 cgal git-extras
 
 # petsc
 #port install petsc slepc
@@ -100,8 +44,8 @@ port install libcryptopp
 # port install synergy
 
 # octave and its packages
-port install atlas +gcc48
-port install octave +gcc48
+port install atlas +gcc49
+port install octave +gcc49
 port install octave-general octave-gsl octave-io octave-linear-algebra octave-missing-functions octave-msh octave-optim octave-plot octave-specfun octave-splines octave-statistics octave-strings octave-struct octave-physicalconstants octave-odepkg
 
 # miscellaneous
