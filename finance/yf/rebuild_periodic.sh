35db4f32342349018af89b0dfed5384d956eb4ad
Benjamin S. Kirk <benjamin.kirk@nasa.gov>
2020-04-30 14:17:46 -0500

    cron-like rebuild utility

---
+++ finance/yf/rebuild_periodic.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#set -x
+
+ofile=tsp_shareprices.xls
+
+# Make all if ${ofile} does not exist
+[ -f ${ofile} ] || make all
+
+# If a recent ${ofile} exists, bail
+t_min=$((5 * 1))
+ffile="$(find . -type f -mmin -${t_min} -name ${ofile} -print)"
+
+#echo $ffile
+#echo "x${ffile}" "x./${ofile}"
+
+[ "x${ffile}" == "x./${ofile}" ] &&  { echo -n "Recent ${ffile}: " ; ls -l ${ffile}; exit 0; }
+
+echo "No recent ${ofile}"
+rm -f ${ofile}
+make ${ofile}
+exit 0
