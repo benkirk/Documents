3e73084fe573a9f32785a4cdef2370607a75c5ed
Benjamin Kirk <benkirk@ucar.edu>
2024-12-17 06:49:14 -0700

    checkpoint

--- it/scripts/mac_manual_backups.sh
+++ it/scripts/mac_manual_backups.sh
@@ -2,7 +2,10 @@
 
 declare -a paths=("/Volumes/WD Passport Data" "/Volumes/Seagate Backup/manual")
 
+topdir="$(pwd)"
+
 for externalpath in "${paths[@]}"; do
+
     [ -d "${externalpath}" ] && \
         echo "manually backing up to ${externalpath}" || continue
 
@@ -13,7 +16,22 @@ for externalpath in "${paths[@]}"; do
 
     # local Photos library to external disk, if one already exists
     [ -d  /Volumes/LocalNoRemoteBackup/Photos\ Library.photoslibrary/ ] && \
-        rsync -axvP --force --delete --ignore-errors \
+        echo "Backing Up Photos..." && \
+        rsync -axvHP --force --delete --ignore-errors \
               /Volumes/LocalNoRemoteBackup/Photos\ Library.photoslibrary/ \
               "${externalpath}"/Photos\ Library.photoslibrary/
+
+    mkdir -p "${externalpath}/home_backup/" && cd "${externalpath}/home_backup/" || continue
+    pwd
+    [ -w "$(pwd)" ] || continue
+    echo "Backing up ${HOME}..."
+    rsync -axvHP --force --delete --ignore-errors \
+          --exclude 'Docker.raw' \
+          --exclude '.conda/' \
+          --exclude '.docker/' \
+          --exclude '.tmp/' \
+          "${HOME}"/ \
+          "$(pwd)"/
+
+    cd ${topdir}
 done
